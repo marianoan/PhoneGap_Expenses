6c5eb4029a3536fc3adc0a08b1c1cb7143b449dd
marianoan <marianoangonzalez@gmail.com>
2013-11-18 16:11:03 -0300

    Add contact Button

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -2,11 +2,12 @@
 
     this.initialize = function () {
         this.el = $('<div/>');
+        this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.add-contact-btn', this.addToContacts);
     };
 
     this.render = function () {
         this.el.html(EmployeeView.template(employee));
-        this.el.on('click', '.add-location-btn', this.addLocation);
         return this;
     };
 
@@ -20,6 +21,24 @@
             function () {
                 alert('Error getting location');
             });
+        return false;
+    };
+
+
+    this.addToContacts = function (event) {
+        event.preventDefault();
+        console.log('addToContacts');
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
+        var contact = navigator.contacts.create();
+        contact.name = { givenName: employee.firstName, familyName: employee.lastName };
+        var phoneNumbers = [];
+        phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+        phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true); // preferred number
+        contact.phoneNumbers = phoneNumbers;
+        contact.save();
         return false;
     };
 
